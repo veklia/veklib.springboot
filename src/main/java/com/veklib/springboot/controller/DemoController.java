831363eb3a2c844a32995228b4b67ce31bc9b85f
veklia <veklia@126.com>
2017-10-28 14:41:08 +0800

    add datasource type. and insert interface.

--- src/main/java/com/veklib/springboot/controller/DemoController.java
+++ src/main/java/com/veklib/springboot/controller/DemoController.java
@@ -4,11 +4,13 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
 import com.veklib.springboot.bean.Demo;
+import com.veklib.springboot.bean.RequestResult;
 import com.veklib.springboot.dao.DemoMapper;
 
 @RestController
@@ -20,10 +22,18 @@ public class DemoController {
 	DemoMapper demoDao;
 	
 	@RequestMapping("/info/{id}")
-	public String insertData(@PathVariable Integer id){
+	public String selectData(@PathVariable Integer id){
 		logger.info("search for id = " + id);
 		Demo demo = demoDao.selectByPrimaryKey(id);
 		return JSONObject.toJSONString(demo);
+	}
+	@RequestMapping("/insert")
+	public String insertData(@RequestBody Demo demo){
+		RequestResult result = new RequestResult();
+		demoDao.insert(demo);
+		result.setResult(0);
+		result.setStatus(200);
+		return JSONObject.toJSONString(result);
 	}
 	
 }
